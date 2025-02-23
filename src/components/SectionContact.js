abc6fa57f42904a64a5fb6cdb34cd4d47ba18de6
Brian Wendt <brian.m.wendt@gmail.com>
2025-02-23 09:33:21 -0700

    Replaced Facebook with Bluesky

--- src/components/SectionContact.js
+++ src/components/SectionContact.js
@@ -1,16 +1,15 @@
 import Section from "./Section";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFacebook, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faBluesky, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import classNames from "classnames";
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 export default function SectionContact(props) {
     return (
         <Section id="contact" className={classNames("text-center", props.className)}>
             <div className="text-5xl">
-                <a href="http://fb.me/OntoDevelopment" className="m-3" target="_blank" rel="noreferrer">
-                    <FontAwesomeIcon icon={faFacebook} />
+                <a href="https://bsky.app/profile/azbrian.bsky.social" className="m-3" target="_blank" rel="noreferrer">
+                    <FontAwesomeIcon icon={faBluesky} />
                 </a>
                 <a href="https://www.linkedin.com/in/brianwendtarizona" className="m-3" target="_blank" rel="noreferrer">
                     <FontAwesomeIcon icon={faLinkedin} />
